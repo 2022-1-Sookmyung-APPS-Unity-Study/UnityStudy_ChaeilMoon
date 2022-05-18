22b66cae6720fb9b48cbbc71ce48b78dd18ea98a
chaei <chaeilmo2002@gmail.com>
2022-05-18 22:10:01 +0900

    7th

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed;
-    public float input;
 
     Rigidbody2D rb;
-    Animator anim;
+    private Animator anim;
+
+    public int health;
 
     // Start is called before the first frame update
     void Start()
@@ -17,15 +18,19 @@ public class Player : MonoBehaviour
         anim = GetComponent<Animator>();
     }
 
-    private void Update()
+
+    // Update is called once per frame
+    void FixedUpdate()
     {
+        float input = Input.GetAxisRaw("Horizontal");
+
         if (input != 0)
         {
             anim.SetBool("isRunning", true);
         }
         else
         {
-            anim.SetBool("isRunning",false);
+            anim.SetBool("isRunning", false);
         }
 
         if (input > 0)
@@ -36,15 +41,20 @@ public class Player : MonoBehaviour
         {
             transform.eulerAngles = new Vector3(0, 180, 0);
         }
-    }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        input = Input.GetAxisRaw("Horizontal");
 
         //moving player
         rb.velocity = new Vector2(input * speed, rb.velocity.y);
-
     }
+
+    public void TakeDamage(int damageAmount)
+    {
+        health -= damageAmount;
+
+        if (health <= 0)
+        {
+            //destroy player
+            Destroy(gameObject);
+        }
+    }
+
 }
